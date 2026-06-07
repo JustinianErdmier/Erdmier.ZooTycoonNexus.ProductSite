097498489368da124842f946f200a111d85898c7
Justinian Erdmier <48148443+JustinianErdmier@users.noreply.github.com>
2026-06-07 03:51:51 -0500

    style(🎨): add nav height comment to scrollToSection

--- Source/Presentation.Web/wwwroot/js/site.js
+++ Source/Presentation.Web/wwwroot/js/site.js
@@ -1,6 +1,6 @@
 window.scrollToSection = function (id) {
     const element = document.getElementById(id);
     if (element) {
-        window.scrollTo({ top: element.getBoundingClientRect().top + window.scrollY - 54, behavior: "smooth" });
+        window.scrollTo({ top: element.getBoundingClientRect().top + window.scrollY - 54, behavior: "smooth" }); // 54 px = sticky nav height
     }
 };
